9759635182754103fe3c5566e3f77419f0519713
Abdallah Abdillah <waploaj@Abdallahs-MacBook-Pro.local>
2020-02-02 12:57:27 +0300

    Enumaration Leacture 3

--- Enumaration.playground/Contents.swift
+++ Enumaration.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var str = "Hello, playground"
+enum WeatherType{
+    
+    case sun
+    case wind(speed:Int)
+    case snow
+    case rain
+    
+}
+
+// Now we ad
+
+func check_weather(weather:WeatherType) -> String?{
+    switch weather {
+    case .sun:
+        return nil
+    case .wind (let speed) where speed < 100:
+        return "The speed of wind is less than 100"
+    case .wind,.rain:
+        return "200"
+    case .snow:
+        return "snow"
+    
+    }
+}
