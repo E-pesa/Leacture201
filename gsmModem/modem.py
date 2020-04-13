74d6db4bdb012647830baa77fcf84da4ff3acd61
Abdallah Abdillah <waploaj@icloud.com>
2020-04-13 23:08:55 +0300

    update huawei api client

--- gsmModem/modem.py
+++ gsmModem/modem.py
@@ -1,7 +1,12 @@
 import huaweisms.api.user as user
 import huaweisms.api.sms as sms
 
-ctx = user.quick_login("admin","admin")
-print(ctx)
-if __name__ == "__main__":
-    print(sms.get_sms(ctx,1,1,1,unread_preferred=True))
+ctx = user.quick_login("admin", "admin")
+meseji = sms.get_sms(ctx,1,1,qty=10,unread_preferred=True)
+
+
+for ujumbe in meseji['response']['Messages']['Message']:
+    if "HaloPesa" in ujumbe["Phone"]:
+        muamala = ujumbe['Content'].split('Transaction')
+        print(muamala)
+
