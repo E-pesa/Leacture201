06a24e479d78bcd22ec465d2a5f474b4076a0dc0
Abdallah Abdillah <waploaj@icloud.com>
2020-04-14 23:41:27 +0300

    send sms via gsm modem

--- gsmModem/modem.py
+++ gsmModem/modem.py
@@ -1,5 +1,6 @@
 import huaweisms.api.user as user
 import huaweisms.api.sms as sms
+import huaweisms.api.ussd as ussd
 
 ctx = user.quick_login("admin", "admin")
 meseji = sms.get_sms(ctx,1,1,qty=10,unread_preferred=True)
@@ -7,6 +8,11 @@ meseji = sms.get_sms(ctx,1,1,qty=10,unread_preferred=True)
 
 for ujumbe in meseji['response']['Messages']['Message']:
     if "HaloPesa" in ujumbe["Phone"]:
-        muamala = ujumbe['Content'].split('Transaction')
+        muamala = ujumbe['Content'].split('Transcation ID:')
         print(muamala)
+
+if __name__ == '__main__':
+    sms.send_sms(ctx,dest='0768090083',msg="hello this is test")
+    ussd.send(ctx,msg='*150*88#')
+    ussd.status(ctx)
 
