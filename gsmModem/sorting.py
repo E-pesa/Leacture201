bcca70362c749ad3efecf29ce5adcef458b99277
Abdallah Abdillah <waploaj@icloud.com>
2020-04-28 20:15:27 +0300

    selection sorting algorithm

--- gsmModem/sorting.py
+++ gsmModem/sorting.py
@@ -1,4 +1,4 @@
-"""implementing sorting algorithm for sorting officer Azampay(sarafu)"""
+"""implementing bubble  sorting algorithm for sorting officer Azampay(sarafu)"""
 
 
 def b_sorting(array):
@@ -13,9 +13,26 @@ def b_sorting(array):
         x = x + 1
         swapped = False
 
-        for i in range(1, n-x):
+        for i in range(1, n):
             if array[i-1] > array[i]:
                 swapping(i-1,i)
                 swapped = True
     return array
+
+"""implementing selection sorting"""
+
+def selection_sorting(array):
+    for i in range(len(array)):
+        minimum = i
+
+        for j in range(i+1,len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+
+        array[minimum], array[i] = array[i],array[minimum]
+    return array
+
+print(selection_sorting([1,4,42,432,2,43,34]))
+
+
 
